091acbc237325740caa5c10c408e2fd2474ed1f8
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-14 16:19:59 +0300

    Add files via upload

---
+++ shutdown-cluster.sh
@@ -0,0 +1,7 @@
+docker stop cassandra-node1
+docker stop cassandra-node2
+docker stop cassandra-node3
+docker rm cassandra-node1
+docker rm cassandra-node2
+docker rm cassandra-node3
+docker network rm dulher-cassandra-network

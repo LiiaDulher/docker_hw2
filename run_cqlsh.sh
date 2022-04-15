9af87cb3321cdfd72c1585155bf8afbbbd7a33de
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-15 10:57:32 +0300

    Update run_cqlsh.sh

--- run_cqlsh.sh
+++ run_cqlsh.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 docker run -it --network dulher-cassandra-network --rm ddl_image
 docker run -it --network dulher-cassandra-network --rm dml_image
 docker run -it --network dulher-cassandra-network --rm check_image
+
+docker rmi ddl_image
+docker rmi dml_image
+docker rmi check_image

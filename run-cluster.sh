6a2b8a8f7cff07b0e010bd7c7b23772542c90fd7
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-15 14:25:05 +0300

    Update run-cluster.sh

--- run-cluster.sh
+++ run-cluster.sh
@@ -2,8 +2,8 @@
 
 docker network create dulher-cassandra-network
 docker run --name cassandra-node1 --network dulher-cassandra-network -d cassandra:latest
-sleep 65s
+sleep 70s
 docker run --name cassandra-node2 --network dulher-cassandra-network -d -e CASSANDRA_SEEDS=cassandra-node1 cassandra:latest
-sleep 65s
+sleep 70s
 docker run --name cassandra-node3 --network dulher-cassandra-network -d -e CASSANDRA_SEEDS=cassandra-node1,cassandra-node2 cassandra:latest
-sleep 65s
+sleep 70s

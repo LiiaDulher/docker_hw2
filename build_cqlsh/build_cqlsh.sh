ae237b749ece63cd8579f71c375e7d45bfe310ee
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-15 11:03:03 +0300

    Update build_cqlsh.sh

--- build_cqlsh/build_cqlsh.sh
+++ build_cqlsh/build_cqlsh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker build -t ddl_image -f Dockerfile1 .
-docker build -t dml_image -f Dockerfile2 .
-docker build -t check_image -f Dockerfile3 .
+docker build -t ddl_image -f build_cqlsh/Dockerfile1 .
+docker build -t dml_image -f build_cqlsh/Dockerfile2 .
+docker build -t check_image -f build_cqlsh/Dockerfile3 .

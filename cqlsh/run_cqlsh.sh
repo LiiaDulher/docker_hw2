bcfb55be91265ed3522c268bc10ce899381fefa4
LiiaDulher <61160259+LiiaDulher@users.noreply.github.com>
2022-04-15 11:16:50 +0300

    Update and rename run_cqlsh.sh to cqlsh/run_cqlsh.sh

--- cqlsh/run_cqlsh.sh
+++ cqlsh/run_cqlsh.sh
@@ -3,7 +3,3 @@
 docker run -it --network dulher-cassandra-network --rm ddl_image
 docker run -it --network dulher-cassandra-network --rm dml_image
 docker run -it --network dulher-cassandra-network --rm check_image
-
-docker rmi ddl_image
-docker rmi dml_image
-docker rmi check_image
